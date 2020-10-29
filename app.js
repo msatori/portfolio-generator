b8367338316d9f947be6ddde3df3c16c29cd2943
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-10-28 22:09:10 -0500

    switching computers

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2);
+// console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-   for (let i = 0; i < profileDataArr.length; i += 1) {
-       console.log(profileDataArr[i]);
-   };
 
-   console.log('================');
 
-   profileDataArr.forEach(profileItem =>console.log(profileItem));
-}
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw new Error(err);
 
-   
-printProfileData(profileDataArgs);
+    console.log('Portfolio Complete! Check out index.html to see the ouput!');
+})
